f02c79b82a8f4c7ccf62203a4e1f27a7f84eec77
Tharushi Subhani <tharushisubani123@gmail.com>
2026-03-18 17:57:59 +0530

    Add files via upload

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 17, 2026 at 06:53 AM
+-- Generation Time: Mar 18, 2026 at 01:26 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -35,6 +35,14 @@ CREATE TABLE `messages` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `name`, `email`, `message`, `created_at`) VALUES
+(1, 'Praveen Theekshana', 'praveentheekshana@gmail.com', 'check', '2026-03-18 11:53:50'),
+(2, 'Tharushi', 'tharushi@gmail.com', 'check 2', '2026-03-18 12:04:31');
+
 -- --------------------------------------------------------
 
 --
@@ -55,7 +63,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `created_at`) VALUES
 (1, 'tharushi subhani', 'tharushisubani123@gmail.com', '$2y$10$Wp5c/jteoeV8TlGTZwN/cepBtk9/kzfCNugDyFB/CqzXvqywRmkxG', '2026-03-17 05:39:52'),
-(2, 'shahan vimukthi', 'shahanvimukthi76@gmail.com', '$2y$10$PXAGMxc.l/AlbyS/obD1FuXAWHawn8PgmyTX5o1e/5hXPfHUOUpSC', '2026-03-17 05:44:59');
+(2, 'shahan vimukthi', 'shahanvimukthi76@gmail.com', '$2y$10$PXAGMxc.l/AlbyS/obD1FuXAWHawn8PgmyTX5o1e/5hXPfHUOUpSC', '2026-03-17 05:44:59'),
+(3, 'Praveen Theekshana', 'praveentheekshana03@gmail.com', '$2y$10$GEpxMZS8dZ5MBqQar1QUaecTDgzkMFHgBtxVkFs2CV2IE9L2lmnuW', '2026-03-18 12:19:30');
 
 --
 -- Indexes for dumped tables
@@ -82,13 +91,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
